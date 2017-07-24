b32da282962368d67b3d3628d8f520b13b1398d9
Warren <warren.euriclide@outlook.fr>
2017-07-24 20:18:05 +0200

    Terminé : Large Hero Block and Intro Content Block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssimport =  require('postcss-import'),
+mixins = require('postcss-mixins');
+
+gulp.task("styles", function(){
+  return gulp.src("./app/assets/styles/styles.css")
+  .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+  // erro handling: en cas d'erreur glulp renvoie des info sur l'erreur
+  .on('error', function(errorInfo){
+    console.log(errorInfo.toString());
+    //mais ne termine pas le processus de watch ou le serveur
+    this.emit('end');
+  })
+  .pipe(gulp.dest('./app/temp/styles/'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    watch = require("gulp-watch"),
+    browserSync = require('browser-sync').create();
+
+gulp.task("watch", ()=>{
+
+  //activer le serveur créer par browserSync qui utilise le dossier "app"
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  //utiliser la method .reload de browserSync chaque fois qu'un changement
+  // est détecter ce le ficher
+  watch("./app/index.html", ()=>{
+    browserSync.reload();
+  });
+
+
+  watch("./app/assets/styles/**/*.css", ()=>{
+    gulp.start("cssInject");
+
+  });
+});
+
+// cette task n'est lancé que si les "dependencies" contenues dans le deuxieme
+//argument ( ici ["styles]" ) n'ont été lancé et terminer
+gulp.task('cssInject',["styles"] , ()=> {
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
